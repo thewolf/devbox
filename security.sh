b3e6d176b0b9b5ade39b4e14413762ae87fbcb17
thewolf <hatem.taleb@gmail.com>
2016-03-21 10:45:51 +0100

    Update security.sh

--- security.sh
+++ security.sh
@@ -5,5 +5,7 @@ set -u
 #enabling the automatic security updates:
 sudo dpkg-reconfigure --priority=low unattended-upgrades
 
+sudo apt-get install -y fail2ban
+
 sudo apt-get update && sudo apt-get dist-upgrade -y && sudo reboot
 
