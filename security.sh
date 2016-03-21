fe955bcccf950f51825acb59a991c8da755ea5d8
thewolf <hatem.taleb@gmail.com>
2016-03-21 10:35:25 +0100

    security.sh

---
+++ security.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+set -u
+
+#enabling the automatic security updates:
+sudo dpkg-reconfigure --priority=low unattended-upgrades
+
+sudo apt-get update && sudo apt-get dist-upgrade -y && sudo reboot
+
